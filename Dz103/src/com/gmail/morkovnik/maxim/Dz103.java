e92cd0fe6386a9424b3ccd6ac2581f4c7e518612
MaximMorkovnik <maxim.morkovnik@gmail.com>
2020-01-06 12:09:28 +0200

    commit

---
+++ Dz103/src/com/gmail/morkovnik/maxim/Dz103.java
@@ -0,0 +1,17 @@
+package com.gmail.morkovnik.maxim;
+
+import java.util.Scanner;
+
+public class Dz103 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the radius of the circle and press Enter");
+        double r = sc.nextDouble();
+        double c = 2 * Math.PI * r;
+        System.out.println("Circumference = " + c);
+        sc.close();
+    }
+
+}
